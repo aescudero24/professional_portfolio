c6cc6fe2f10c2ec25cebae3c42b68e98808264c3
Adrian Escudero <adrianescudero1305@gmail.com>
2024-01-31 15:23:48 -0600

    new portfolio finished

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
-const wrapper = document.getElementById("wrapper");
-const mode_button = document.getElementById("dark_light");
-const header = document.querySelector(".header");
-const footer = document.querySelector(".footer");
-const brake = document.querySelector(".brake");
-mode_button.textContent = mode_button.value;
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 50,
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    stagePadding: 250,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 5,
+      },
+      1400: {
+        items: 3,
+      },
+    },
+  });
+});
 
-mode_button.addEventListener("click", () => {
-  if (mode_button.value === "Dark Mode") {
-    mode_button.value = "Light Mode";
-    mode_button.style.border = "3px solid black";
-    mode_button.style.color = "white";
-    wrapper.style.backgroundColor = "black";
-    wrapper.style.color = "white";
-    header.style.backgroundColor = "white";
-    footer.style.backgroundColor = "white";
-    brake.style.backgroundColor = "black";
-  } else {
-    mode_button.value = "Dark Mode";
-    mode_button.style.border = "3px solid white";
-    mode_button.style.color = "black";
-    wrapper.style.backgroundColor = "white";
-    wrapper.style.color = "black";
-    header.style.backgroundColor = "black";
-    brake.style.backgroundColor = "white";
-    footer.style.backgroundColor = "black";
+document.addEventListener("DOMContentLoaded", function () {
+  function toggleTheme() {
+    document.body.classList.toggle("dark-mode");
   }
-  mode_button.textContent = mode_button.value;
+
+  const themeSwitchBtn = document.createElement("button");
+  themeSwitchBtn.id = "themeSwitchBtn";
+  themeSwitchBtn.className = "btn btn-outline-light";
+  themeSwitchBtn.innerHTML = "Toggle Theme";
+  themeSwitchBtn.addEventListener("click", toggleTheme);
+
+  const navbarNav = document.getElementById("navbarNav");
+  navbarNav.appendChild(themeSwitchBtn);
 });
